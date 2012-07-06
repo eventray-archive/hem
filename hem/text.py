28191f7d8a58d809822d024b3fcfc5264b9a8db4
John Anderson <sontek@gmail.com>
2012-07-06 09:27:55 -0500

    added slugify and tests, use py.test runner

--- hem/text.py
+++ hem/text.py
@@ -1,3 +1,5 @@
+from unicodedata import normalize
+
 import hashlib
 import random
 import string
@@ -13,6 +15,21 @@ def generate_random_string(length):
     m.update(word)
 
     return unicode(m.hexdigest()[:length])
+
+
+def slugify(text,
+        encoding=None,
+        permitted_chars='abcdefghijklmnopqrstuvwxyz0123456789-'
+    ):
+    """ Original code: https://gist.github.com/1428479 """
+    if isinstance(text, str):
+        text = text.decode(encoding or 'ascii')
+    clean_text = text.strip().replace(' ', '-').lower()
+    while '--' in clean_text:
+        clean_text = clean_text.replace('--', '-')
+    ascii_text = normalize('NFKD', clean_text).encode('ascii', 'ignore')
+    strict_text = map(lambda x: x if x in permitted_chars else '', ascii_text)
+    return ''.join(strict_text)
 
 
 def pluralize(singular):
--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@ import os
 
 from setuptools import setup, find_packages
 
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
 here = os.path.abspath(os.path.dirname(__file__))
 README = ''#open(os.path.join(here, 'README.md')).read()
 CHANGES = ''#open(os.path.join(here, 'CHANGES.txt')).read()
@@ -33,6 +45,7 @@ setup(name='hem'
       , zip_safe=False
       , install_requires=requires
       , tests_require=requires + ['pytest', 'mock', 'webtest', 'pytest-cov']
+      , cmdclass = {'test': PyTest}
       , test_suite='hem'
 )
 
