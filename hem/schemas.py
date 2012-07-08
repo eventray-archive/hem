1c7358ce9127861dd40a1d971bfa870a47c22b34
John Anderson <sontek@gmail.com>
2012-07-08 14:34:34 -0500

    added a CSRFSchema

---
+++ hem/schemas.py
@@ -0,0 +1,25 @@
+import colander
+
+@colander.deferred
+def deferred_csrf_default(node, kw):
+    request = kw.get('request')
+    csrf_token = request.session.get_csrf_token()
+    return csrf_token
+
+@colander.deferred
+def deferred_csrf_validator(node, kw):
+    def validate_csrf(node, value):
+        request = kw.get('request')
+        csrf_token = request.session.get_csrf_token()
+
+        if value != csrf_token:
+            raise ValueError('Bad CSRF token')
+    return validate_csrf
+
+class CSRFSchema(colander.Schema):
+    csrf = colander.SchemaNode(
+        colander.String(),
+        default = deferred_csrf_default,
+        validator = deferred_csrf_validator,
+        widget = deform.widget.HiddenWidget(),
+    )
