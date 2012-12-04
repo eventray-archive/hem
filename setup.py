111afa4d61de7ac8a225bc14f3064978de818651
John Anderson <sontek@gmail.com>
2012-12-04 15:54:26 -0300

    bump version for release

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
 ]
 
 setup(name='hem',
-    version='dev',
+    version='0.3',
     description='Generic utilities to help build frameworks on top of pyramid',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
