a26570f9336bc56b9eccc08ebc5d34db2f6b1ee1
John Anderson <sontek@gmail.com>
2013-02-08 16:03:57 -0500

    some py3 support

--- hem/schemas.py
+++ hem/schemas.py
@@ -4,6 +4,7 @@ from __future__ import (absolute_import, division, print_function,
     unicode_literals)
 import colander
 import deform
+import six
 
 
 @colander.deferred
@@ -18,15 +19,20 @@ def deferred_csrf_validator(node, kw):
     def validate_csrf(node, value):
         request = kw.get('request')
         csrf_token = request.session.get_csrf_token()
+
+        if six.PY3:
+            csrf_token = csrf_token.decode('utf-8')
+
         if value != csrf_token:
-            raise colander.Invalid(node, 'Bad CSRF token')
+            raise colander.Invalid(node,
+                                   'Invalid cross-site scripting token')
     return validate_csrf
 
 
 class CSRFSchema(colander.Schema):
-    csrf = colander.SchemaNode(
+    csrf_token = colander.SchemaNode(
         colander.String(),
+        widget=deform.widget.HiddenWidget(),
         default=deferred_csrf_default,
         validator=deferred_csrf_validator,
-        widget=deform.widget.HiddenWidget(),
     )
